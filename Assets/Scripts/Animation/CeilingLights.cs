6e5e6a4a9298a78eee2b8e8923886653e694b980
helmiamsani <48036579+helmiamsani@users.noreply.github.com>
2019-08-23 15:21:52 +1000

    boom

---
+++ Assets/Scripts/Animation/CeilingLights.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingLights : MonoBehaviour
+{
+    public Animator lightAnimator;
+    public float lightDelay;
+
+
+    private float time;
+    private float firstDelay = 0f;
+    private int chance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if(time > firstDelay)
+        {
+            firstDelay = time + lightDelay;
+
+            chance = Random.Range(0, 2);
+            if(chance == 0)
+            {
+                lightAnimator.SetBool("isTwiceOn", true);
+            }
+            else
+            {
+                lightAnimator.SetBool("isTwiceOn", false);
+            }
+            
+        }
+    }
+}
